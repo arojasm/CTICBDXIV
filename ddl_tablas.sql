a523093f5118f236c48cca04b44e0f8b50f40357
Arturo <arrome10@gmail.com>
2020-10-03 07:53:19 -0500

    version 5

--- ddl_tablas.sql
+++ ddl_tablas.sql
@@ -1,12 +1,12 @@
 nombre: arturo rojas
 fecha : 27/09
-objetivo : carga de tabla clientes
+objetivo : carga de tabla clientes minorista
 
 CREATE TABLA T_CLIENTE(
 NOMBRE VARCHAR2(10)
 DNI CHAR(8)
 )
-
+tablespace tmp;
 
 
 
